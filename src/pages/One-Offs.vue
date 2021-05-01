b5b60ae20b4b5ebfb1f96efffe6feab41ff91b25
Connor <connor.graham@capgemini.com>
2021-05-01 09:41:46 +0200

    added one off page and template

---
+++ src/pages/One-Offs.vue
@@ -0,0 +1,52 @@
+<template>
+  <Layout>
+   <div>
+      <h1 >One-offs</h1>
+      <ul>
+          <li
+           v-for="one-off in $page.one-off.edges" 
+          :key="one-off.id" 
+          ></li>
+      </ul>
+    </div>
+
+  </Layout>
+</template>
+
+<page-query>
+  query One-offs {
+    one-off: allOneOff {
+      edges {
+        node {
+          id
+          title
+          path
+        }
+      }
+    }
+  }
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'One-Offs'
+  },
+  data() {
+    return {
+    }
+  },
+  created() {
+    // this.$page.one-offs.edges.forEach(item => {
+    //   const parsedDate = new Date(item.node.date).toDateString();
+    //   item.node.date = parsedDate;
+    // });
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+  
+</style>
---
+++ src/templates/One-off.vue
@@ -0,0 +1,32 @@
+<template>
+    <Layout>
+        <g-image :src="$page.one-off.image" width="500"/>
+        <h1>{{$page.one-off.title}}</h1>
+        <p>{{$page.one-off.size}}</p>
+        <p>{{$page.one-off.date}}</p>
+        <p>{{$page.one-off.description}}</p>
+    </Layout>
+</template>
+
+<page-query>
+    query One-Off ($path: String!) {
+        one-off: one-off (path: $path) {
+            title
+            image
+            size
+            date
+            description
+        }
+    }
+</page-query>
+
+<script>
+export default {
+    metaInfo() {
+        return {
+            title: this.$page.one-off.title
+        }
+    }
+}
+</script>
+
