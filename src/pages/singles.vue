2b073def6de2c5d7b8fb63d926580b29ad5a337c
Connor <connor.graham@capgemini.com>
2021-05-07 08:43:57 +0200

    fixed mobile layout and added grid to singles

--- src/pages/singles.vue
+++ src/pages/singles.vue
@@ -9,11 +9,15 @@
                         v-for="(single, i) in $page.single.edges"
                         :key="i"
                     >
-                        <g-link :to="single.node.path">
+                        <g-link class="single__link" :to="single.node.path">
                             <g-image
                                 class="single__image"
                                 :src="single.node.image"
                             />
+                            <h1 class="single__image-heading">
+                                {{ single.node.title }}
+                            </h1>
+                            <div class="single__image-overlay"></div>
                         </g-link>
                     </li>
                 </ul>
@@ -29,6 +33,7 @@
         node {
             path
             image
+            title
         }
       }
     }
@@ -49,15 +54,71 @@ export default {
 </script>
 
 <style lang="scss">
+//https://css-tricks.com/piecing-together-approaches-for-a-css-masonry-layout/
+@import "../assets/main.scss";
 .singles {
     list-style-type: none;
+
+    @media only screen and (min-width: 600px) {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-column-gap: 0px;
+        grid-row-gap: 0px;
+    }
 }
 
 .single {
+    position: relative;
+    z-index: 1;
+
+    &:hover {
+        .single__image-overlay {
+            opacity: 0.25;
+        }
+
+        .single__image-heading {
+            @media only screen and (min-width: 600px) {
+                opacity: 1;
+            }
+        }
+    }
 }
 
 .single__image {
     width: 100%;
     height: auto;
+}
+
+.single__image-overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: #000;
+    z-index: 2;
+    top: 0;
+    left: 0;
+    opacity: 0.25;
+    transition: opacity 0.2s linear;
+
+    @media only screen and (min-width: 600px) {
+        opacity: 0;
+    }
+}
+
+.single__image-heading {
+    z-index: 3;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    text-transform: uppercase;
+    font-size: 42px;
+    color: $white;
+    transition: opacity 0.2s linear;
+    margin: 0;
+
+    @media only screen and (min-width: 600px) {
+        left: 28%;
+        opacity: 0;
+    }
 }
 </style>
